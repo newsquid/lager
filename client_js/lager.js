7c3211e4671fa064c862561324f0198544cb64b4
bladtman <sigurt91@gmail.com>
2014-10-18 15:34:39 +0200

    Dropped the pretend-object abstraction, as it used global scope for variables. Now using a regular map instead. Also: added browser meta-data

--- client_js/lager.js
+++ client_js/lager.js
@@ -1,15 +1,25 @@
 var Lager = (function() {
     var Lager= {
+        //these setters are really unnecessary, but data abstraction is nice.
         setServer: function(addr) {
-            server = addr;
+            //function sets the remote logging server address
+            Lager.server = addr;
         },
         setOrigin: function(identifier) {
-            origin = identifier;
+            //function sets an ID for the platform. Eg. name of service using Lager
+            Lager.origin = identifier;
         },
+        meta: JSON.stringify({
+            UA: navigator.userAgent,
+            lang: navigator.languages,
+            cookiesEnabled: navigator.cookieEnabled,
+            platform: navigator.platform
+        }),
         log: function(message) {
-            var pars = encodeURI("?msg=" + message + "&origin=" + origin);
+            //function logs message and meta-data to the Lager server
+            var pars = encodeURI("?msg=" + message + "&origin=" + Lager.origin + "&meta-data=" + Lager.meta);
             xhr = new XMLHttpRequest();
-            xhr.open('HEAD', server + pars);
+            xhr.open('HEAD', Lager.server + pars);
             xhr.send();
         }
     }
